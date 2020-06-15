7d0a434899661a2940d4158a442fd664bdb968c6
William Stevens <wstevens.hornet@gmail.com>
2020-06-15 00:34:46 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   Rails.application.routes.draw do
   get "hello_world", to: "static#hello_world"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  end
 
 end
